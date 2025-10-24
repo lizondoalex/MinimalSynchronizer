d8f4bfc2bce3785dd27483be0b5db9a10231322c
LizondoPC <lizondoalex@gmail.com>
2025-10-25 01:26:15 +0200

    reformatting a bit and advancing, added config option. Next step is to connect to server

--- ms.c
+++ ms.c
@@ -22,7 +22,6 @@ get_current_date(json_object *result) {
   json_object_object_add(result, "tm_wday",  json_object_new_int(current_time->tm_wday));
   json_object_object_add(result, "tm_yday",  json_object_new_int(current_time->tm_yday));
   json_object_object_add(result, "tm_isdst", json_object_new_int(current_time->tm_isdst));
-
 }
 
 static void
@@ -62,13 +61,11 @@ ensure_directory_exists(const char *path) {
   } else {
     printf("Creating directory %s\n", path);
   }
-  printf("Finished creating directories\n");
 }
 
 
 static void
 *get_default_config_path(char *buffer, size_t buffer_size) {
-  puts("getting default config path");
 
   const char *home_dir = getenv("HOME");
 
@@ -78,7 +75,6 @@ static void
   }
 
   snprintf(buffer, buffer_size, "%s/.config/minisync/config.jsonc", home_dir);
-
 }
 
 static FILE
@@ -110,8 +106,6 @@ static json_object
 
   json_object *default_json = json_object_new_object();
   get_current_date(default_json);
-
-
 }
 
 static void
@@ -158,22 +152,12 @@ usage() {
   exit(1);
 }
 
-static struct tm
+static json_object*
 get_config_date() {
   json_object *config = read_config();
-
-  json_object *j_sec, *j_min, *j_hour, *j_mday, *j_mon, *j_year, *j_wday, *j_yday, *j_isdst;
-
-  json_object_object_get_ex(config, "tm_sec",   &j_sec);
-  json_object_object_get_ex(config, "tm_min",   &j_min);
-  json_object_object_get_ex(config, "tm_hour",  &j_hour);
-  json_object_object_get_ex(config, "tm_mday",  &j_mday);
-  json_object_object_get_ex(config, "tm_mon",   &j_mon);
-  json_object_object_get_ex(config, "tm_year",  &j_year);
-  json_object_object_get_ex(config, "tm_wday",  &j_wday);
-  json_object_object_get_ex(config, "tm_yday",  &j_yday);
-  json_object_object_get_ex(config, "tm_isdst", &j_isdst);
-
+  json_object *result = json_object_new_object();
+  json_object_object_get_ex(config, "time", &result);
+  return result;
 }
 
 static void
@@ -194,61 +178,30 @@ status() {
 
 }
 
+static void
+diff() {
+
+}
+
 int main(int argc, char **argv){
-
-  update_date();
-
-  json_object *json = read_config();
-  char *config_json_string = json_object_to_json_string_ext(json, JSON_C_TO_STRING_PRETTY);
-  printf("current config: %s\n", config_json_string);
-  json_object *time = json_object_new_object();
-  get_current_date(time);
-  char *s = json_object_to_json_string_ext(time, JSON_C_TO_STRING_PRETTY);
-  printf("current time = %s\n", s);
-
-  return 1;
-
 
   if (argc == 1) {
     usage();
   }
 
   for (int i = 1; i < argc; i++) {
-
     if (!strcmp(argv[i], "status")) {
       status();
-      return 0;
-    } if (!strcmp(argv[i], "init")) {
+    }else if (!strcmp(argv[i], "init")) {
       create_default_config();
-      return 0;
+    }else if (!strcmp(argv[i], "diff")) {
+      diff();
+    }else if (!strcmp(argv[i], "config")) {
+      char path[1024];
+      get_default_config_path(path, sizeof(path));
+      printf("The path to the config file is: %s\n", path);
     }
 
   }
   return 1;
-
-
-/*
-  struct json_object *ip;
-
-  if (!json_object_object_get_ex(new_parsed_json, "ip", &ip)) {
-    fprintf(stderr, "Error finding key 'ip' in JSON.\n");
-    json_object_put(new_parsed_json);
-    return 1;
-  }
-
-  if (!json_object_is_type(ip , json_type_string)) {
-    fprintf(stderr, "Error: Key \"name\" is not a string.\n");
-    json_object_put(parsed_json);
-    return 1;
-  }
-
-  const char *name_ip = json_object_get_string(ip);
-  printf("your ip is %s\n", name_ip);
-
-  json_object_put(parsed_json);
-
-  return 0;
-
-  */
-
 }
